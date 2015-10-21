cb89eb85e63124087d24b4820a17e8be5511b9d8
Xianfeng Yuan <xyuan@samsung.com>
2015-10-20 23:36:38 -0700

    add timer

--- app/components/Deployments.jsx
+++ app/components/Deployments.jsx
@@ -7,21 +7,49 @@ import util from 'util';
 import hp from '../utils/helpers';
 import DeployTable from './DeployTable';
 
+let INTERVAL = 10000;
+
 export default class Deployments extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
             show: false,
             cols: props.cols,
+            ts: new Date().toString(),
             content: {}
         }
     }
 
+    componentWillMount() {
+        this.intervals = [];
+    }
+    
+    componentWillUnmount() {
+        this.intervals.map(clearInterval);
+    }
+    
+    tick() {
+        if (this.state.show) {
+            hp.getJSON('deploy', this.props, function(data) {
+                this.setState({
+                    content: data.content,
+                    cols: data.cols,
+                    ts: new Date().toString()
+                });
+            }.bind(this));
+        }
+    }
+
+    componentDidMount() {
+        this.intervals.push(setInterval(this.tick.bind(this), INTERVAL));
+    }
+    
     showModal() {
         hp.getJSON('deploy', this.props, function(data) {
             this.setState({
                 content: data.content,
                 cols: data.cols,
+                ts: new Date().toString(),
                 show: true
             });
         }.bind(this));
@@ -45,6 +73,7 @@ export default class Deployments extends React.Component {
                     <Modal.Footer>
                         <Button onClick={this.hideModal.bind(this)}>Close</Button>
                     </Modal.Footer>
+                    <Button bsStyle="link">Data requested at: {this.state.ts}</Button>
                 </Modal>
             </ButtonToolbar>
         )
